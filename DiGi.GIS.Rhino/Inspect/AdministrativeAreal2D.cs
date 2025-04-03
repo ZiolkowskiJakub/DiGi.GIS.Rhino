8f550b356ffab8bfa27d85836ac4217f163d0cdc
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-04-03 07:35:38 +0200

    References updated

--- DiGi.GIS.Rhino/Inspect/AdministrativeAreal2D.cs
+++ DiGi.GIS.Rhino/Inspect/AdministrativeAreal2D.cs
@@ -17,27 +17,5 @@ namespace DiGi.GIS.Rhino
 
             return new GH_String(administrativeAreal2D.Name);
         }
-
-        [Inspect("Occupancy", "Occupancy", "AdministrativeAreal2D Occupancy")]
-        public static GH_Integer Occupancy(this AdministrativeAreal2D administrativeAreal2D)
-        {
-            if (administrativeAreal2D == null || administrativeAreal2D.Occupancy == null)
-            {
-                return null;
-            }
-
-            return new GH_Integer(System.Convert.ToInt32(administrativeAreal2D.Occupancy.Value));
-        }
-
-        [Inspect("AdministrativeArealType", "AdministrativeArealType", "AdministrativeAreal2D AdministrativeArealType")]
-        public static GooEnum<AdministrativeArealType> AdministrativeArealType(this AdministrativeAreal2D administrativeAreal2D)
-        {
-            if (administrativeAreal2D == null)
-            {
-                return null;
-            }
-
-            return new GooEnum<AdministrativeArealType>(administrativeAreal2D.AdministrativeArealType);
-        }
     }
 }
---
+++ DiGi.GIS.Rhino/Inspect/AdministrativeSubdivision.cs
@@ -0,0 +1,43 @@
+﻿using DiGi.GIS.Classes;
+using DiGi.GIS.Enums;
+using DiGi.Rhino.Core.Classes;
+using Grasshopper.Kernel.Types;
+
+namespace DiGi.GIS.Rhino
+{
+    public static partial class Inspect
+    {
+        [Inspect("Name", "Name", "AdministrativeSubdivision Name")]
+        public static GH_String Name(this AdministrativeSubdivision administrativeSubdivision)
+        {
+            if (administrativeSubdivision == null)
+            {
+                return null;
+            }
+
+            return new GH_String(administrativeSubdivision.Name);
+        }
+
+        [Inspect("Occupancy", "Occupancy", "AdministrativeSubdivision Occupancy")]
+        public static GH_Integer Occupancy(this AdministrativeSubdivision administrativeSubdivision)
+        {
+            if (administrativeSubdivision == null || administrativeSubdivision.Occupancy == null)
+            {
+                return null;
+            }
+
+            return new GH_Integer(System.Convert.ToInt32(administrativeSubdivision.Occupancy.Value));
+        }
+
+        [Inspect("AdministrativeSubdivisionType", "AdministrativeSubdivisionType", "AdministrativeSubdivision AdministrativeSubdivisionType")]
+        public static GooEnum<AdministrativeSubdivisionType> AdministrativeSubdivisionType(this AdministrativeSubdivision administrativeSubdivision)
+        {
+            if (administrativeSubdivision == null)
+            {
+                return null;
+            }
+
+            return new GooEnum<AdministrativeSubdivisionType>(administrativeSubdivision.AdministrativeSubdivisionType);
+        }
+    }
+}
