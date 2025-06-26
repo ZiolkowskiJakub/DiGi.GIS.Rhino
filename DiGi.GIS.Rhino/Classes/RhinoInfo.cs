176489b467300538ce077d94bdb14b4eba9c0a95
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-06-26 22:16:37 +0200

    RhinoInfo added

---
+++ DiGi.GIS.Rhino/Classes/RhinoInfo.cs
@@ -0,0 +1,25 @@
+﻿using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+
+namespace DiGi.GIS.Rhino.Classes
+{
+    public class RhinoInfo : GH_AssemblyInfo
+    {
+        public override string Name => "DiGi.GIS.Rhino";
+
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        public override Bitmap Icon => null;
+
+        //Return a short string describing the purpose of this GHA library.
+        public override string Description => "";
+
+        public override Guid Id => new Guid("0eba0ac9-8220-41a3-a177-061d6ccfdaa4");
+
+        //Return a string identifying you or your company.
+        public override string AuthorName => "Jakub Ziolkowski";
+
+        //Return a string representing your preferred contact details.
+        public override string AuthorContact => "jakubziolkowski@digiproject.uk";
+    }
+}
